cb2f602f2a906e4363236285eb987b91a836c066
DongHyeonka <ehdgus6435@kumoh.ac.kr>
2024-08-29 22:45:41 +0900

    refactor : 여러가지 암호화 기술을 사용할 수 있도록 빈 등록 수정

--- src/main/java/com/creativedesignproject/kumoh_board_backend/auth/config/AuthConfig.java
+++ src/main/java/com/creativedesignproject/kumoh_board_backend/auth/config/AuthConfig.java
@@ -1,15 +1,15 @@
-package com.creativedesignproject.kumoh_board_backend.config;
+package com.creativedesignproject.kumoh_board_backend.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoderConfig {
+public class AuthConfig {
     
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
